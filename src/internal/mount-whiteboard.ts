96b5d9d3f416aee03cb3cf7654fb50e9574524a5
hyrious <hyrious@outlook.com>
2021-11-18 16:21:54 +0800

    refactor: add some default room options

--- src/internal/mount-whiteboard.ts
+++ src/internal/mount-whiteboard.ts
@@ -1,38 +1,48 @@
 import type { JoinRoomParams, WhiteWebSdkConfiguration } from "white-web-sdk";
 import type { MountParams } from "@netless/window-manager";
 
-import { WhiteWebSdk } from "white-web-sdk";
+import { DefaultHotKeys, WhiteWebSdk } from "white-web-sdk";
 import { WindowManager } from "@netless/window-manager";
 
-export type JoinRoom = Omit<JoinRoomParams, "useMultiViews" | "disableMagixEventDispatchLimit">;
-
-export type WhiteWindowSDKConfiguration = Omit<WhiteWebSdkConfiguration, "useMobXState">;
-
 export interface MountWhiteboardParams {
-  sdkConfig: WhiteWindowSDKConfiguration;
-  joinRoom: JoinRoom;
+  sdkConfig: Omit<WhiteWebSdkConfiguration, "useMobXState">;
+  joinRoom: Omit<JoinRoomParams, "useMultiViews" | "disableMagixEventDispatchLimit">;
   managerConfig?: Omit<MountParams, "room" | "container">;
 }
 
 export async function mountWhiteboard(
-  { sdkConfig, joinRoom, managerConfig = { cursor: true } }: MountWhiteboardParams,
+  { sdkConfig, joinRoom, managerConfig }: MountWhiteboardParams,
   container: HTMLDivElement
 ) {
   const sdk = new WhiteWebSdk({
     ...sdkConfig,
-    appIdentifier: sdkConfig.appIdentifier,
     useMobXState: true,
   });
 
   const room = await sdk.joinRoom({
+    floatBar: true,
+    disableNewPencil: false,
+    disableMagixEventDispatchLimit: true,
+    hotKeys: {
+      ...DefaultHotKeys,
+      changeToSelector: "s",
+      changeToLaserPointer: "z",
+      changeToPencil: "p",
+      changeToRectangle: "r",
+      changeToEllipse: "c",
+      changeToEraser: "e",
+      changeToText: "t",
+      changeToStraight: "l",
+      changeToArrow: "a",
+      changeToHand: "h",
+    },
     ...joinRoom,
     invisiblePlugins: [WindowManager],
     useMultiViews: true,
-    disableNewPencil: false,
-    disableMagixEventDispatchLimit: true,
   });
 
   const manager = await WindowManager.mount({
+    cursor: true,
     ...managerConfig,
     room,
     container,
