9b02c6a36d7e42e7ae596c5f958b814af64fb3c2
hyrious <hyrious@outlook.com>
2022-01-05 16:57:42 +0800

    refactor: export internals

--- src/WhiteboardApp.ts
+++ src/WhiteboardApp.ts
@@ -10,16 +10,50 @@ export type { WhiteboardAppConfig, InsertDocsParams };
 export class WhiteboardApp {
   private readonly _instance: Instance;
 
+  private _target: HTMLElement | null = null;
+  public get target(): HTMLElement | null {
+    return this._target;
+  }
+
   constructor(readonly config: WhiteboardAppConfig) {
     this._instance = new Instance(config);
   }
 
+  get room() {
+    return this._instance.room;
+  }
+
+  get manager() {
+    return this._instance.manager;
+  }
+
+  get sdk() {
+    return this._instance.sdk;
+  }
+
+  get i18n() {
+    return this._instance.i18n;
+  }
+
   public bindElement(target?: HTMLElement | null) {
+    this._target = target || null;
     this._instance.target = target || null;
   }
 
   public insertDocs(params: InsertDocsParams) {
     return this._instance.insertDocs(params);
+  }
+
+  public insertCodeEditor() {
+    return this._instance.insertCodeEditor();
+  }
+
+  public insertGeoGebra() {
+    return this._instance.insertGeoGebra();
+  }
+
+  public insertCountdown() {
+    return this._instance.insertCountdown();
   }
 
   public changeLanguage(language: Language) {
