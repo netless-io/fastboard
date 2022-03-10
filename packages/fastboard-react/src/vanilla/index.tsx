3a7ccc5be8f20c6abbf399b1ffce03616f24c6ed
hyrious <hyrious@outlook.com>
2022-03-10 14:40:18 +0800

    refactor: make mount() props optional

--- packages/fastboard-react/src/vanilla/index.tsx
+++ packages/fastboard-react/src/vanilla/index.tsx
@@ -8,17 +8,17 @@ import { Fastboard } from "../components/Fastboard";
 export type MountProps = Omit<FastboardProps & DivProps, "ref">;
 
 /**
- * Mount fastboard app to some dom, returns the disposer, which will unmount the app.
+ * Mount fastboard app to some dom, returns the updater and disposer.
  * @example
  * let app = await createFastboard({ ...config })
  * const { update, destroy } = mount(app, document.getElementById("whiteboard"))
  * update({ theme: 'dark' })
  * destroy()
  */
-export function mount(app: FastboardApp, dom: HTMLElement, props: MountProps) {
+export function mount(app: FastboardApp, dom: HTMLElement, props: MountProps = {}) {
   ReactDOM.render(<Fastboard app={app} {...props} />, dom);
   return {
-    update(props: MountProps) {
+    update(props: MountProps = {}) {
       ReactDOM.render(<Fastboard app={app} {...props} />, dom);
     },
     destroy() {
