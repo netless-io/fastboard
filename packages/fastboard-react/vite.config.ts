b813b837c4e1b30086cf20e4c89d30ff84916848
hyrious <hyrious@outlook.com>
2022-07-04 16:25:42 +0800

    chore: remove dedupe from vite react plugin

--- packages/fastboard-react/vite.config.ts
+++ packages/fastboard-react/vite.config.ts
@@ -1,9 +1,18 @@
+import type { Plugin } from "vite";
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import react from "@vitejs/plugin-react";
 
+const viteReactPlugins = react();
+// Remove `dedupe: ['react', 'react-dom']` from the viteReactRefresh plugin.
+const viteReactRefresh = viteReactPlugins.find(
+  e => (e as Plugin | null)?.name === "vite:react-refresh"
+) as Plugin;
+delete viteReactRefresh.config;
+
 export default defineConfig({
-  plugins: [react(), svelte()],
+  plugins: [viteReactPlugins, svelte()],
   envDir: "../..",
   define: { __NAME__: '"@netless/fastboard"', __VERSION__: '"develop"' },
 });
