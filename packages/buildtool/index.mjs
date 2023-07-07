13263121c49145d1ef7aece0093cb5f8ca23a2b8
hyrious <hyrious@outlook.com>
2023-07-07 15:25:07 +0800

    chore: add "svelte" field

--- packages/buildtool/index.mjs
+++ packages/buildtool/index.mjs
@@ -13,53 +13,63 @@ import { sass } from "@netless/esbuild-plugin-inline-sass";
  *   name: string,
  *   version: string,
  *   main: string,
+ *   svelte?: string,
  *   dependencies?: Record<string, string>,
  *   peerDependencies?: Record<string, string>
  * }} pkg
  */
-export async function build({ dir, main, version, dependencies, peerDependencies }) {
+export async function build({
+  dir,
+  name,
+  main,
+  svelte: svelteOutfile,
+  version,
+  dependencies,
+  peerDependencies,
+}) {
   process.chdir(dir);
   fs.rmSync("dist", { recursive: true, force: true });
 
-  let start = Date.now();
-  const bundle = await rollup.rollup({
-    input: main,
-    plugins: [
-      {
-        name: "esbuild",
-        async load(id) {
-          const { outputFiles } = await esbuild.build({
-            entryPoints: [id],
-            bundle: true,
-            format: "esm",
-            // The filename here does not really matter, because
-            // rollup will then merge sourcemaps and get the original input filename.
-            outfile: id.replace(/\.tsx?$/, ".js"),
-            sourcemap: true,
-            write: false,
-            target: ["es2017"],
-            plugins: [svelte(), sass()],
-            loader: {
-              ".svg": "dataurl",
-            },
-            define: {
-              __NAME__: '"@netless/fastboard"',
-              __VERSION__: JSON.stringify(version),
-            },
-            external: Object.keys({
-              ...dependencies,
-              ...peerDependencies,
-            }),
-          });
-          let code, map;
-          for (const { path, text } of outputFiles) {
-            if (path.endsWith(".map")) map = text;
-            else code = text;
-          }
-          return { code, map };
+  const esbuildPlugin = external => ({
+    name: "esbuild",
+    async load(id) {
+      const { outputFiles } = await esbuild.build({
+        entryPoints: [id],
+        bundle: true,
+        format: "esm",
+        // The filename here does not really matter, because
+        // rollup will then merge sourcemaps and get the original input filename.
+        outfile: id.replace(/\.tsx?$/, ".js"),
+        sourcemap: true,
+        write: false,
+        target: ["es2017"],
+        plugins: [svelte(), sass()],
+        loader: {
+          ".svg": "dataurl",
         },
-      },
-    ],
+        define: {
+          __NAME__: '"@netless/fastboard"',
+          __VERSION__: JSON.stringify(version),
+        },
+        external: Object.keys({
+          ...dependencies,
+          ...peerDependencies,
+          ...(external && external.reduce((acc, cur) => ((acc[cur] = true), acc), {})),
+        }),
+      });
+      let code, map;
+      for (const { path, text } of outputFiles) {
+        if (path.endsWith(".map")) map = text;
+        else code = text;
+      }
+      return { code, map };
+    },
+  });
+
+  let start = Date.now();
+  let bundle = await rollup.rollup({
+    input: main,
+    plugins: [esbuildPlugin()],
     external: [/^[@a-z]/],
   });
 
@@ -80,6 +90,24 @@ export async function build({ dir, main, version, dependencies, peerDependencies
   await cjs;
   await bundle.close();
   console.log("Built dist/index.{js|mjs} in", Date.now() - start + "ms");
+
+  // Generate dist/index.svelte.mjs for the UI package,
+  // this also helps to avoid depending on svelte in fastboard/fastboard-react test.
+  if (name.endsWith("-ui")) {
+    start = Date.now();
+    let bundle = await rollup.rollup({
+      input: main,
+      plugins: [esbuildPlugin(["svelte"])],
+      external: [/^[@a-z]/],
+    });
+    await bundle.write({
+      file: svelteOutfile,
+      format: "es",
+      sourcemap: true,
+    });
+    await bundle.close();
+    console.log("Built dist/index.svelte.mjs in", Date.now() - start + "ms");
+  }
 
   start = Date.now();
   await dts.build(main, "dist/index.d.ts", { exclude: ["svelte", "svelte/internal"] });
--- packages/fastboard/vite.config.mts
+++ packages/fastboard/vite.config.mts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  plugins: [svelte({ preprocess: vitePreprocess() })],
   envDir: "../..",
   define: { __NAME__: '"@netless/fastboard"', __VERSION__: '"develop"' },
 });
