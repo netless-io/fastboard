584f4cad01b1762da37ae829ed17886cec08907e
hqer927 <285569480@qq.com>
2024-09-21 10:34:26 +0800

    chore:release 0.3.19

--- packages/fastboard-react/test/replay.tsx
+++ packages/fastboard-react/test/replay.tsx
@@ -24,7 +24,9 @@ function App() {
       },
       replayRoom: {
         room: import.meta.env.VITE_ROOM_UUID || "b0c80aa0770011ef83863d0682a6c9bd",
-        roomToken: import.meta.env.VITE_ROOM_TOKEN || "NETLESSROOM_YWs9VWtNUk92M1JIN2I2Z284dCZleHBpcmVBdD0xNzI2ODg5NDMyMzc3Jm5vbmNlPWIwZWM4MjkwLTc3MDAtMTFlZi05NmE5LWFiMzg4NjE4OThhZiZyb2xlPTEmc2lnPTMzZGU0MDQ2ZDg5YzNkNDliOTFkMGQwZDYwOTY3MWIzYzU5NWQzN2IxNTFiZDhkM2Y2ZjYwYjFmODUwMWYxODQmdXVpZD1iMGM4MGFhMDc3MDAxMWVmODM4NjNkMDY4MmE2YzliZA",
+        roomToken:
+          import.meta.env.VITE_ROOM_TOKEN ||
+          "NETLESSROOM_YWs9VWtNUk92M1JIN2I2Z284dCZleHBpcmVBdD0xNzI2ODg5NDMyMzc3Jm5vbmNlPWIwZWM4MjkwLTc3MDAtMTFlZi05NmE5LWFiMzg4NjE4OThhZiZyb2xlPTEmc2lnPTMzZGU0MDQ2ZDg5YzNkNDliOTFkMGQwZDYwOTY3MWIzYzU5NWQzN2IxNTFiZDhkM2Y2ZjYwYjFmODUwMWYxODQmdXVpZD1iMGM4MGFhMDc3MDAxMWVmODM4NjNkMDY4MmE2YzliZA",
         beginTimestamp: 1726884289991,
         duration: 36000,
         callbacks: {
@@ -36,7 +38,7 @@ function App() {
       managerConfig: {
         cursor: true,
       },
-      enableAppliancePlugin: true
+      enableAppliancePlugin: true,
     }).then(player => {
       (window as any).player = player;
       (window as any).forceUpdate = forceUpdate;
